d637b672cd23652698b285042a23596c8dcd277a
Kendra Rodrigues <105830575+KendraRodrigues@users.noreply.github.com>
2022-12-09 15:57:06 -0300

    Arquivo de controle do banco,
    para realizar ações com o banco

---
+++ CONTROLE.sql
@@ -0,0 +1,8 @@
+USE sistema_tcc
+
+INSERT INTO alunos (`aluno_matricula`,`nome`,
+`ano_ingresso`,`email`,`endereco`,`telefones`,`curso_id` )
+ VALUES ('10160112', 'Steicy', '2021', '10160112@instituição', 'Rua tal numero 000',
+ '15655922154', '3';
+
+ 
